2e58b4ac32c1be34d217bdadd14c54eb85ca0992
HuanjunWang <153147512@qq.com>
2018-08-22 23:11:04 +0800

    InvertedPendulum Fine

--- hw2/train_pg_v2.py
+++ hw2/train_pg_v2.py
@@ -25,7 +25,8 @@ class MyArgument(object):
                  seed=1,
                  n_layers=1,
                  size=32,
-                 debug=False):
+                 debug=False,
+                 train_size=100):
         self.exp_name = exp_name
         self.env_name = env_name
         self.n_iter = n_iter
@@ -36,6 +37,7 @@ class MyArgument(object):
         self.reward_to_go = reward_to_go
         self.render = render
         self.debug = debug
+        self.train_size = train_size
 
         self.normalize_advantage = normalize_advantage
         self.nn_baseline = nn_baseline
@@ -49,7 +51,9 @@ class MyArgument(object):
         self.log_dir += 'rtg_' if self.reward_to_go else ''
         self.log_dir += 'norm_' if self.normalize_advantage else ''
         self.log_dir += 'nn_%d_%d_' % (self.n_layers, self.size)
-        self.log_dir += 'lr_%6f' % self.learning_rate
+        self.log_dir += 'lr_%6f_' % self.learning_rate
+        self.log_dir += 'batch_%d_' % self.min_batch_size
+        self.log_dir += 't_%d' % self.train_size
 
         self.log_dir = os.path.join(base_dir, self.log_dir)
         self.log_dir = os.path.join(self.log_dir, 'seed%d' % self.seed)
@@ -97,27 +101,42 @@ class PgModelContinuous(PgModel):
             # N x action dim
             # Output activation ?
             self.action_mean = self.build_mlp(input_placeholder=self.ph_observation,
-                                              output_size=self.action_dim, scope="Mean_%d_%d" % (n_layers, size),
-                                              size=size, n_layers=n_layers)
+                                              output_size=self.action_dim,
+                                              scope="Mean_%d_%d" % (n_layers, size),
+                                              size=size,
+                                              n_layers=n_layers,
+                                              activation=tf.nn.relu,
+                                              output_activation=None)
 
             # action dim
-            self.action_sigma = tf.get_variable(name='Sigma', shape=[self.action_dim], dtype=tf.float32, trainable=True)
+            # self.action_sigma = tf.get_variable(name='Sigma', shape=[self.action_dim],
+            #                                     dtype=tf.float32, trainable=True, initializer=tf.ones_initializer())
+
+            self.action_sigma = self.build_mlp(input_placeholder=self.ph_observation,
+                                               output_size=self.action_dim,
+                                               scope="Sigma_%d_%d" % (n_layers, size),
+                                               size=32,
+                                               n_layers=1,
+                                               activation=tf.nn.relu,
+                                               output_activation=tf.nn.sigmoid)
 
             tf.summary.histogram('Mean', self.action_mean)
             tf.summary.histogram('Std', self.action_sigma)
 
             # Broadcast expected here
             # Get N x action dim distributions
-            self.normal_dist = tf.distributions.Normal(self.action_mean, self.action_sigma, name="PredictDistribution")
+            self.normal_dist = tf.distributions.Normal(self.action_mean, (self.action_sigma + 1e-8),
+                                                       name="PredictDistribution")
 
             # Expected N* action dis distributions
             self.predict_action = self.normal_dist.sample(name="PredictAction")
-            #self.predict_action = tf.clip_by_value(normal_dist.sample(), env.action_space.low, env.action_space.high,
+            # self.predict_action = tf.clip_by_value(normal_dist.sample(), env.action_space.low, env.action_space.high,
             #                                      name="PredictAction")
 
             with tf.name_scope("Loss"):
-                action_prob = self.normal_dist.log_prob(self.ph_action, name="Prob")
-                self.actor_loss = - tf.reduce_mean(action_prob * self.ph_advance)
+                self.action_prob = self.normal_dist.log_prob(self.ph_action, name="Prob")
+                self.actor_loss = - tf.reduce_mean(self.action_prob * tf.expand_dims(self.ph_advance, -1))
+                tf.summary.scalar('Actor Loss', self.actor_loss)
         with tf.name_scope("Opt/"):
             self.action_opt = tf.train.AdamOptimizer(learning_rate).minimize(self.actor_loss)
 
@@ -145,13 +164,14 @@ class PgModelContinuous(PgModel):
 
     def get_predict_action(self, sess, observation):
         action = sess.run(self.predict_action, feed_dict={self.ph_observation: observation[None]})
-        return action
+        return action[0]
 
     def update(self, sess, observations, actions, q, normalize_advance, mean_reward):
         if self.nn_baseline:
-            # Update Cirtic Network
-            sess.run(self.critic_opt, feed_dict={self.ph_observation: observations,
-                                                 self.ph_q_value: q})
+            # Update Critic Network
+            for i in range(10):
+                sess.run(self.critic_opt, feed_dict={self.ph_observation: observations,
+                                                     self.ph_q_value: q})
             baseline = sess.run(self.predict_baseline, feed_dict={self.ph_observation: observations})
             advance = q - baseline
         else:
@@ -224,33 +244,35 @@ class PgModelDiscrete(PgModel):
 
     def get_predict_action(self, sess, observation):
         action = sess.run(self.predict_action, feed_dict={self.ph_observation: observation[None]})
-        return action
+        return action[0]
 
     def update(self, sess, observations, actions, q, normalize_advance, mean_reward):
         if self.nn_baseline:
             # Update Cirtic Network
-            sess.run(self.critic_opt, feed_dict={self.ph_observation: observations,
-                                                 self.ph_q_value: q})
+            for i in range(int(observations.shape[0] / 500)):
+                sess.run(self.critic_opt, feed_dict={self.ph_observation: observations,
+                                                     self.ph_q_value: q})
             baseline = sess.run(self.predict_baseline, feed_dict={self.ph_observation: observations})
             advance = q - baseline
         else:
             advance = q.copy()
 
         if normalize_advance:
-            advance = (advance - np.mean(advance)) / np.std(advance)
+            advance = (advance - np.mean(advance)) / (np.std(advance) + 1e-8)
 
         # Update the Actor network
-        if self.debug:
-            _, summary = sess.run([self.action_opt, self.merged], feed_dict={self.ph_observation: observations,
-                                                                             self.ph_action: actions,
-                                                                             self.ph_advance: advance,
-                                                                             self.ph_q_value: q,
-                                                                             self.ph_mean_reward: mean_reward})
-        else:
-            sess.run(self.action_opt, feed_dict={self.ph_observation: observations,
-                                                 self.ph_action: actions,
-                                                 self.ph_advance: advance})
-            summary = None
+        for i in range(int(observations.shape[0] / 500)):
+            if self.debug:
+                _, summary = sess.run([self.action_opt, self.merged], feed_dict={self.ph_observation: observations,
+                                                                                 self.ph_action: actions,
+                                                                                 self.ph_advance: advance,
+                                                                                 self.ph_q_value: q,
+                                                                                 self.ph_mean_reward: mean_reward})
+            else:
+                sess.run(self.action_opt, feed_dict={self.ph_observation: observations,
+                                                     self.ph_action: actions,
+                                                     self.ph_advance: advance})
+                summary = None
 
         return summary
 
@@ -277,13 +299,55 @@ def discount_reward(paths, gamma, reward_to_go):
     q_n = np.array(discounted_reward, dtype=np.float32)
     return q_n
 
+
 def verify_model(sess, model, env):
     action_dim = env.action_space.shape[0]
     observation_dim = env.observation_space.shape[0]
 
-    observations = np.random.randn(100, observation_dim)
-    actions = model.get_predict_action()
+    observations = np.random.randn(observation_dim)
+    print("Observation shape:", observations.shape)
+    print(observations)
+    actions = model.get_predict_action(sess, observations)
+    print("action shape:", actions.shape)
+    print(actions)
+    assert actions.shape == (action_dim,)
 
+    N = 10
+    observations = np.random.randn(N, observation_dim)
+    print("Observation shape:", observations.shape)
+    print(observations)
+    predict_action = sess.run(model.predict_action, feed_dict={model.ph_observation: observations})
+    print("Action shape:", predict_action.shape)
+    print(predict_action)
+    assert predict_action.shape == (N, action_dim)
+
+    action_prob = sess.run(model.action_prob, feed_dict={model.ph_observation: observations,
+                                                         model.ph_action: predict_action})
+    print("Prob:", action_prob)
+    assert action_prob.shape == (N, action_dim)
+
+    loss = sess.run(model.actor_loss, feed_dict={model.ph_observation: observations,
+                                                 model.ph_action: predict_action,
+                                                 model.ph_advance: np.ones(N)})
+    print("Loss:", loss)
+
+    for i in range(20):
+        sess.run(model.action_opt, feed_dict={model.ph_observation: observations,
+                                              model.ph_action: predict_action,
+                                              model.ph_advance: np.ones(N)})
+
+    action_prob2 = sess.run(model.action_prob, feed_dict={model.ph_observation: observations,
+                                                          model.ph_action: predict_action})
+    print("Prob2:", action_prob2)
+    assert action_prob2.shape == (N, action_dim)
+
+    loss2 = sess.run(model.actor_loss, feed_dict={model.ph_observation: observations,
+                                                  model.ph_action: predict_action,
+                                                  model.ph_advance: np.ones(N)})
+    print("Loss2:", loss2)
+    assert loss2 < loss
+    print(np.sum(action_prob2 > action_prob))
+    assert np.sum(action_prob2 > action_prob) == N * action_dim
 
 
 def train_pg(args):
@@ -313,9 +377,8 @@ def train_pg(args):
     tf.global_variables_initializer().run()  # pylint: disable=E1101
     writer = tf.summary.FileWriter(args.log_dir, sess.graph)
 
-    if not discrete:
-        verify_model(sess, model, env)
-
+    # if not discrete:
+    #     verify_model(sess, model, env)
 
     max_path_length = args.max_path_length or env.spec.max_episode_steps
     for itr in range(args.n_iter):
@@ -334,7 +397,7 @@ def train_pg(args):
                     time.sleep(0.0001)
 
                 obs.append(observation)
-                action = model.get_predict_action(sess, observation)[0]
+                action = model.get_predict_action(sess, observation)
                 acs.append(action)
                 observation, rew, done, _ = env.step(action)
                 rewards.append(rew)
@@ -371,16 +434,33 @@ def main():
     for baseline in [True]:
         for normalize in [True]:
             for reward_to_go in [True]:
-                for seed in [8]:
-                    env_name = 'CartPole-v0'
-                    env_name = 'MountainCar-v0'
-                    env_name = 'MountainCarContinuous-v0'
-                    args = MyArgument(env_name=env_name, seed=seed, debug=True, n_layers=1,
-                                      size=32, reward_to_go=reward_to_go, normalize_advantage=normalize,
-                                      nn_baseline=baseline, n_iter=1000)
-                    p = Process(target=train_pg, args=(args,))
-                    p.start()
-                    p.join()
+                for lr in [5e-4, 3e-4]:
+                    for min_batch in [2000, 1000]:
+                        for train_size in [400]:
+                            for seed in [77]:
+                                env_name = 'CartPole-v0'
+                                env_name = 'MountainCar-v0'
+                                env_name = 'MountainCarContinuous-v0'
+                                env_name = 'InvertedPendulum-v1'
+                                # env_name = "Ant-v1"
+                                env_name = 'HalfCheetah-v1'
+                                args = MyArgument(env_name=env_name,
+                                                  seed=seed,
+                                                  debug=True,
+                                                  n_layers=3,
+                                                  size=128,
+                                                  reward_to_go=reward_to_go,
+                                                  normalize_advantage=normalize,
+                                                  nn_baseline=baseline,
+                                                  n_iter=50,
+                                                  learning_rate=lr,
+                                                  render=False,
+                                                  max_path_length=40,
+                                                  min_batch_size=min_batch,
+                                                  train_size=train_size)
+                                p = Process(target=train_pg, args=(args,))
+                                p.start()
+                                p.join()
 
 
 if __name__ == "__main__":
